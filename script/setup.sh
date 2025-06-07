28eb28a971e116407cae7777b29decf41ba55665
Cruzy22k <zakisullivan63@gmail.com>
2025-06-07 15:37:30 +1000

    rollback

--- script/setup.sh
+++ script/setup.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 #this is a draft of the full script. 
-
 useradd -m -s /bin/bash admin
 echo "admin:admin123" | chpasswd
 usermod -aG wheel admin
 #This sections just sets up a functioning admin acc on the vm. (assuming you just set up root in the setup)
-
 #gets sshd running
 dnf install -y openssh-server
 systemctl enable sshd
@@ -27,50 +25,38 @@ setfacl -m u:ctfadmin000:rx /root
 # -m is for modify, u is for the user, r for read, x for execute, (for 
 # directory travel) /root is the dir that we are talking about to the compter.
 
-#SETUP FOR OS_CLIPRIV
-
-chmod 544 /home/os_clipriv000 # 500 is for read and ex for user
-echo "echo 'c2VjdXJlcGFzczEyMw==' | base64 --decode" > /home/os_clipriv000/.bash_history
-chown root:root /home/os_clipriv000/.bash_history
-chmod 400 /home/os_clipriv000/.bash_history
-setfacl -m u:os_clipriv000:r /home/os_clipriv000/.bash_history #readonly
-
-# this restricts the clipriv000 account to just read and execute, BUT only # in their home dir.
+chmod 700 /home/os_clipriv000 
+# this restricts the clipriv000 account to just read, write, execute, BUT only # in their home dir. it also stops other vm users from seeing stuff in there.
+echo "echo 'c2VjdXJlcGFzczEyMw==' | base64 --decode" >> /home/os_clipriv000/.bash_history
+# this adds the 'mistake' command left in the bash history)
+chown os_clipriv000:os_clipriv000 /home/os_clipriv000/.bash_history
+chmod 600 /home/os_clipriv000/.bash_history
 # we are also gonna add a hint file explaining what base64 is, and how to access its manpage. # its owned by root for best practice
-
-echo -e "Welcome player. This challenge teaches you how to use base64, and how chain attack and escalation of permissions can lead to massively scaling attack vectors.\nHint: look around, snoop in the history... learn how base64 works... good luck!\nRead the Challenge Markdown for more info." > /home/os_clipriv000/note.txt
-chmod 400 /home/os_clipriv000/note.txt
+echo -e "Welcome player. This challenge teaches you how to use base64, and how chain attack and escalation of permissions can lead to massively scaling attack vectors. \nHint: look around, snoop in the history... learn how base64 works... good luck! \n Read the Challenge Markdown for more infomation" > /home/os_clipriv000/note.txt
 chown root:root /home/os_clipriv000/note.txt
+chown root:root /home/os_clipriv000/ -R
+chown root:root /home/os_clipriv000/
+chmod 400 /home/os_clipriv000/note.txt
 setfacl -m u:os_clipriv000:r /home/os_clipriv000/note.txt
-chown root:root /home/os_clipriv000
-chmod 500 /home/os_clipriv000
-
-# fake crap coz its cool
 echo "did some sussy things, progressing to the next stage"
 sleep 2
 echo "proccessing..."
 sleep 2
-
-### SETUP FOR CTFADMIN000
-#this is a hint file for people when they get access to ctfadmin000
-echo "I think there's something important in /root..." > /home/ctfadmin000/note.txt  # creates a note file
-chown root:root /home/ctfadmin000/note.txt #makes the file owned by root
-chmod 400 /home/ctfadmin000/note.txt # read-only
-setfacl -m u:ctfadmin000:r /home/ctfadmin000/note.txt # lets the fake admin account read it
-chmod 500 /home/ctfadmin000 # owner can read , owner can ex
-chown root:root /home/ctfadmin000 # owner as root
-
-# creates the flag file
+# these commands are for realisicness, first one sets the owner to clipriv000, # and the second one makes it so that only the owner can read/write.
 echo "CTF{a195d2ed41c0da0dfa1d08e196bff242}" | base64 > /root/flag.txt
-chown root:root /root/flag.txt # flag is owned by root
-chmod 440 /root/flag.txt
-# sets permissions to '4' read for owner only (root), 4 for group, 0 for others
+chmod 640 /root/flag.txt 
+# sets permissions to '6' rw for owner, '4' r for group, '0' nothing for others
+chown root:root /root/flag.txt
+# makes root own this file coz we aint stupid we don wan some random deleting  # it or changing it.
 setfacl -m u:ctfadmin000:r /root/flag.txt
-# here is where the magic happens, the ':r' allows ctfadmin read access of this flag, while still keeping the environment secure
+#this is a hint file for people when they get access to ctfadmin000
+echo "I think there's something important in /root..." > /home/ctfadmin000/note.txt
+echo "I think there's something importalnt in /root..." > /home/ctfadmin000/note.txt
+chown root:root /home/ctfadmin000/note.txt
+chmod 400 /home/ctfadmin000/note.txtAdd commentMore actions
+setfacl -m u:ctfadmin000:r /home/ctfadmin000/note.txt
 
-#chmod 500 /root
-
-#rm -r /root/script.sh
-#echo "removed the script file"
-
+rm -r /root/script.sh
+echo "removed the script file"
+# here is where the magic happens, the ':r' allows ctfadmin read access of this # flag, why still keeping the enviroment secure
 echo "Setup complete. Reboot if needed."
